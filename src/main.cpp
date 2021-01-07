35154df3fb34e0f7dd042ba7eb04fe83f3f593f6
Wouter Ensink <wouterensink3@gmail.com>
2021-01-07 13:57:51 +0100

    made first versions of joystick and display controllers

--- src/main.cpp
+++ src/main.cpp
@@ -46,10 +46,8 @@ void loop()
  ***** Please set the number of devices you have *****
  But the maximum default of 8 MAX72XX wil also work.
  */
-LedControl lc = LedControl (12, 11, 10, 4);
 
 /* we always wait a bit between updates of the display */
-unsigned long delaytime = 500;
 
 /*
  This time we have more than one device.
@@ -89,7 +87,7 @@ void loop()
 //constexpr auto joyStick2XPin = A3;
 //constexpr auto joyStick2YPin = A4;
 //constexpr auto joyStick2ButtonPin = 4;
-
+/*
 constexpr auto joyStick2XPin = A5;
 constexpr auto joyStick2YPin = A6;
 constexpr auto joyStick2ButtonPin = 2;
@@ -105,14 +103,6 @@ struct SequencerDisplay
     static constexpr int numRows = 8;
     static constexpr int numColumnsPerHardwareDisplay = 8;
     static constexpr int numSequencerColumns = 32;
-
-    template <typename Functor>
-    void forEachPixel (Functor&& callback)
-    {
-        for (auto row = 0; row < numRows; ++row)
-            for (auto column = 0; column < numColumnsPerHardwareDisplay * numDisplays; ++column)
-                callback (*this, displayBuffer[row][column]);
-    }
 
     void setup()
     {
@@ -136,7 +126,7 @@ struct SequencerDisplay
                 {
                     auto col = column + display * numColumnsPerHardwareDisplay;
                     auto value = displayBuffer[row][col];
-                    ledController.setLed (display, row, 7-column, value);
+                    ledController.setLed (display, row, 7 - column, value);
                 }
             }
         }
@@ -172,7 +162,26 @@ struct SequencerDisplay
             turnLedOff (r, col);
     }
 
-    int displayBuffer[numRows][numColumnsPerHardwareDisplay * numDisplays] = {};
+    void turnColumnOn (int col)
+    {
+        for (auto r = 0; r < numRows; ++r)
+            turnLedOn (r, col);
+    }
+
+    void turnRowOn (int row)
+    {
+        for (auto c = 0; c < numSequencerColumns; ++c)
+            turnLedOn (row, c);
+    }
+
+    void turnOff()
+    {
+        for (auto row = 0; row < numRows; ++row)
+            for (auto c = 0; c < numSequencerColumns; ++c)
+                displayBuffer[row][c] = false;
+    }
+
+    bool displayBuffer[numRows][numColumnsPerHardwareDisplay * numDisplays] = {};
     LedControl ledController { 12, 11, 10, numDisplays };
 };
 
@@ -209,11 +218,185 @@ void loop()
         {
             display.turnLedOn (i, c);
             display.render();
-            delay (10);
+            delay (5);
         }
-        display.turnColumnOff (c);
+        display.turnColumnOff(c);
     }
 
+    //display.turnOff();
+   // display.render();
 
     //delay (500);
+}
+ */
+
+//We always have to include the library
+
+constexpr auto dataInPin = 12;
+constexpr auto clkPin = 11;
+constexpr auto loadPin = 10;
+constexpr auto numDisplays = 8;
+
+auto ledControl = LedControl { dataInPin, clkPin, loadPin, numDisplays };
+
+unsigned long delaytime = 500;
+
+// =====================================================================================
+
+struct JoyStick
+{
+    struct PinLayout
+    {
+        int analogXPin;
+        int analogYPin;
+        int buttonClickPin;
+    };
+
+    enum struct Direction
+    {
+        left,
+        right,
+        up,
+        down
+    };
+
+    struct Listener
+    {
+        virtual void joyStickUpdate (JoyStick&, Direction) = 0;
+    };
+
+    explicit JoyStick (const PinLayout& layout) : pinLayout (layout)
+    {
+    }
+
+    void setup() const
+    {
+        pinMode (pinLayout.analogXPin, INPUT);
+        pinMode (pinLayout.analogYPin, INPUT);
+        pinMode (pinLayout.buttonClickPin, INPUT);
+    }
+
+    void update()
+    {
+        if (listener == nullptr)
+            return;
+
+        constexpr auto halfRes = resolution / 2;
+
+        auto xVal = analogRead (pinLayout.analogXPin);
+        auto yVal = analogRead (pinLayout.analogYPin);
+        auto buttonDown = (bool) digitalRead (pinLayout.buttonClickPin);
+
+        auto absX = abs (xVal - halfRes);
+        auto absY = abs (xVal - halfRes);
+
+        if (absX > threshold)
+        {
+            if (absY > absX)
+                direction = yVal > halfRes ? Direction::up : Direction::down;
+            else
+                direction = xVal > halfRes ? Direction::right : Direction::left;
+        }
+
+        listener->joyStickUpdate (*this, direction);
+    }
+
+    const PinLayout pinLayout;
+    Listener* listener = nullptr;
+    static constexpr auto resolution = 1024;
+    static constexpr auto threshold = 400;
+    Direction direction = Direction::down;
+};
+
+// =====================================================================================
+
+// the display is like one long display of 8x64 broken in two
+//
+struct Display_16x32
+{
+    struct Spec
+    {
+        int dataInPin;
+        int clkPin;
+        int loadPin;
+        int numDisplays;
+    };
+
+    struct Pixel
+    {
+        int x;
+        int y;
+    };
+
+    explicit Display_16x32 (const Spec& spec) : spec (spec) {}
+
+    void setup()
+    {
+        for (int display = 0; display < spec.numDisplays; display++)
+        {
+            ledController.shutdown (display, false);
+            ledController.setIntensity (display, 8);
+            ledController.clearDisplay (display);
+        }
+    }
+
+    void turnOnPixel (const Pixel& pixel) noexcept
+    {
+        setStateOfPixel (pixel, true);
+    }
+
+    void turnOffPixel (const Pixel& pixel) noexcept
+    {
+        setStateOfPixel (pixel, false);
+    }
+
+private:
+    Spec spec;
+
+    LedControl ledController {
+        spec.dataInPin,
+        spec.clkPin,
+        spec.loadPin,
+        spec.numDisplays
+    };
+
+    void setStateOfPixel (const Pixel& pixel, bool state)
+    {
+        auto x = 7 - (pixel.x % 8);  //+ 32 * (pixel.x >= 32);
+        auto y = 7 - (pixel.y % 8);
+        auto display = (pixel.y / 8) + (pixel.x / 8) * 4;
+        ledController.setLed (display, x, y, state);
+    }
+};
+
+// =====================================================================================
+
+const auto displaySpec = Display_16x32::Spec {
+    .dataInPin = dataInPin,
+    .clkPin = clkPin,
+    .loadPin = loadPin,
+    .numDisplays = numDisplays
+};
+
+auto display = Display_16x32 { displaySpec };
+
+void setup()
+{
+    display.setup();
+}
+
+void loop()
+{
+    for (int x = 0; x < 16; ++x)
+    {
+        for (int y = 0; y < 32; ++y)
+        {
+            display.turnOnPixel ({ x, y });
+            delay (100);
+            display.turnOffPixel ({ x, y });
+            delay (100);
+        }
+    }
+
+    //display.turnOnPixel({ 0, 0});
 }
